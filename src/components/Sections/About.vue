584f516a2657dad064a71dfe54522a5c85dcbcd3
semajame <jamesanquillano@gmail.com>
2024-07-19 15:28:27 +0800

    update

--- src/components/Sections/About.vue
+++ src/components/Sections/About.vue
@@ -26,7 +26,7 @@ export default {
         "Hi! I'm a passionate Frontend Developer from Cebu, Philippines. I specialize in crafting visually appealing and user-friendly interfaces with the use of my technology stack.",
 
       secondPara:
-        "  My dedication to frontend development thrives on the dynamic challenges of staying at the forefront of technology. I also like to stay in the latest of technology, and learn about them more on my free time. My journey is driven by the mission of making the digital world a better place one project at a time.",
+        "  My dedication to frontend development thrives on the dynamic challenges of staying at the forefront of technology. I also like to stay in the latest of technology, and learn about them more on my free time. I am currently making my thesis project using Angular.",
     };
   },
 };
